dee74f92b18669e1e49218c456c541e5c1b8ba4f
Ujaan Das <ujaandas03@gmail.com>
2026-01-13 14:23:43 +0000

    some comments

--- main.cpp
+++ main.cpp
@@ -7,35 +7,46 @@ extern "C" void hideAppByPID(pid_t pid);
 extern "C" void unhideAppByPID(pid_t pid);
 
 int main() {
+  // Track seen PIDs
   std::unordered_set<pid_t> seen;
 
+  // Get list of visible windows
   CFArrayRef windowList =
       CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID);
 
+  // Get number of visible windows
   int numWindows = CFArrayGetCount(windowList);
 
+  // Iterate over windows
   for (int i = 0; i < numWindows; i++) {
+
+    // Get window dict
     const CFDictionaryRef windowInfo =
         (CFDictionaryRef)CFArrayGetValueAtIndex(windowList, i);
 
+    // Get window PID
+    int pid = 0;
     const CFNumberRef windowPid =
         (CFNumberRef)CFDictionaryGetValue(windowInfo, kCGWindowOwnerPID);
-    int pid = 0;
     CFNumberGetValue(windowPid, kCFNumberIntType, &pid);
 
+    // Skip VSCodium and seen PIDs
     if (seen.count(pid) || pid == 33410) {
       continue;
     }
 
-    seen.insert(pid);
-
+    // Get window name
+    char name[256];
     const CFStringRef windowName =
         (CFStringRef)CFDictionaryGetValue(windowInfo, kCGWindowOwnerName);
-    char name[256];
     CFStringGetCString(windowName, name, sizeof(name), kCFStringEncodingUTF8);
 
+    // Hide window
     printf("%s-> PID %d\n", name, pid);
     hideAppByPID(pid);
+
+    // Add to list of seen
+    seen.insert(pid);
   }
 
   CFRelease(windowList);
