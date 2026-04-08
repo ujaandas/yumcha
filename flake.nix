6764ca8551cecdf37bdf63ffba0fbf810d3db4a1
Ujaan Das <ujaandas03@gmail.com>
2026-04-08 13:31:24 +0000

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              go-tools
+            ];
+          };
+        };
+    };
+}
