8e1e0a6285d230150b65977499cb4f94a473f387
Ujaan Das <ujaandas03@gmail.com>
2026-04-08 18:09:43 +0100

    write darwin screens api

--- internal/platform/platform_darwin.go
+++ internal/platform/platform_darwin.go
@@ -2,7 +2,10 @@
 
 package platform
 
-import "github.com/ujaandas/yumcha/internal/core"
+import (
+	"github.com/progrium/darwinkit/macos/appkit"
+	"github.com/ujaandas/yumcha/internal/core"
+)
 
 type darwinPlatform struct{}
 
@@ -10,7 +13,27 @@ func defaultPlatform() Platform {
 	return darwinPlatform{}
 }
 
-func (darwinPlatform) Screens() ([]core.Screen, error)        { panic("Unimplemented!") }
+func (darwinPlatform) Screens() ([]core.Screen, error) {
+	akScreens := appkit.Screen_Screens()
+
+	screens := make([]core.Screen, 0, len(akScreens))
+
+	for i, v := range akScreens {
+		// TODO: query DeviceDescription() with String_StringWithUTF8String("NSScreenNumber")
+		frame := v.Frame()
+		screens = append(screens, core.Screen{
+			ID: i,
+			Rect: core.Rect{
+				X:      int(frame.Origin.X),
+				Y:      int(frame.Origin.Y),
+				Width:  int(frame.Size.Width),
+				Height: int(frame.Size.Height),
+			},
+		})
+	}
+	return screens, nil
+}
+
 func (darwinPlatform) FocusedWindow() (core.Window, error)    { panic("Unimplemented!") }
 func (darwinPlatform) Windows() ([]core.Window, error)        { panic("Unimplemented!") }
 func (darwinPlatform) VisibleWindows() ([]core.Window, error) { panic("Unimplemented!") }
